4723df43ec22362ce83b56435fab46e4b7fde1da
Juan Jose huertas <jjhuertasbotache@gmail.com>
2024-06-23 23:08:15 -0500

    testing whats I

--- whatsapp_bot/whatsapp_bot_functions.py
+++ whatsapp_bot/whatsapp_bot_functions.py
@@ -21,9 +21,9 @@ def whats_login():
   shutil.move('screenshot.png', 'temp/screenshot.png')
   #move the file to the temp folder
 
-# def main():
-#   # get the current hr and min
-#   now = datetime.datetime.now()
-#   hr = now.hour
-#   mi = now.minute + 1
-#   pywk.sendwhatmsg("+573012167977","hi",hr,mi)
+def main():
+  # get the current hr and min
+  now = datetime.datetime.now()
+  hr = now.hour
+  mi = now.minute + 1
+  pywk.sendwhatmsg("+573012167977","hi",hr,mi)
