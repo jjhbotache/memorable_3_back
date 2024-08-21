4dbac7b1f2fd300b1fa77cd2280ddd5d6b34ce6f
jjhbotache <jjhuertasbotache@gmail.com>
2024-08-21 10:22:58 -0500

    attemp to deploy on vercel

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Instalar Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable -y
+
+# Configurar el entorno de Rust
+source $HOME/.cargo/env
+
+# Instalar las dependencias de Python
+pip install -r requirements.txt
